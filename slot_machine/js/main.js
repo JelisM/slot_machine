85bc5af4a0af38372ab7da51e145044d35759ce0
JelisM <jelismaiara@gmail.com>
2023-01-04 17:20:17 -0500

    initial comment

---
+++ slot_machine/js/main.js
@@ -0,0 +1,60 @@
+/*----- constants -----*/ 
+
+let player =  null;
+
+
+
+
+
+/*----- app's state (variables) -----*/ 
+
+let start, reset
+
+
+/*----- cached element references -----*/ 
+const h2El = document.querySelector('h2')
+const boxdEl = document.getElementById('box')
+const resetBtnEl = document.createElement('button')
+
+
+
+/*----- event listeners -----*/ 
+boardEl.addEventListener('click', handleBoxClick)
+
+
+/*----- functions -----*/
+
+function render() {
+    renderMessage()
+    renderBoard()
+    if ((winner) || !board.includes(null)){
+        renderResetBtn()
+
+    }
+
+}
+
+function init() {
+
+
+
+}
+
+function getRandomNbr() {
+
+    return Math.floor(Math.random()*2) + 2;
+}
+
+function getElement() {
+    return document.getElementById();
+
+
+}
+
+function spin() {
+
+    const item1  = getElement('item1');
+    const item2  = getElement('item2');
+    const item3  = getElement ('item3');
+}
+
