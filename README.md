b3b98e7b2aa245bc09cc9133990082788e73e87e
JelisM <jelismaiara@gmail.com>
2023-01-05 21:19:59 -0500

    final

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-test 
+# Slot Machine Game
+
+This is a very basic slot machine game that was made using HTML, CSS and JAvascript.
+It has 3 slots that contains different numbers. The number range is from 1 to 4.
+
+This game has two states (win)  (lose). When you win you will see a message saying "JACKPOT" when you loose the message will be TRY AGAIN
+it has 2 buttons one to PLAY and one RESTART that refreshes the page.
+
+Here are some images on how the game works.
--- js/main.js
+++ js/main.js
@@ -1,53 +1,57 @@
-let boardState = 0
+
 const h2El = document.querySelector('h2')
 const boxEl = document.getElementById('slots')
 const playBtn = document.getElementById('spinner')
-const resetBtn = document.getElementById('reseter')
+const resetBtn = document.getElementById('reset')
 const item1 = document.getElementById('numberOne')
 const item2 = document.getElementById('numberTwo')
 const item3 = document.getElementById('numberThree')
 
 
-playBtn.addEventListener('click', spin)
-//resetBtn.addEventListener('click', reset)
+playBtn.addEventListener('click', renderMessage)
 
+playBtn.addEventListener('click', hideMessage)
 
-
-
+resetBtn.addEventListener('click', reset)
 
 function getRandomNbr() {
-
-    return Math.floor(Math.random()*(4)) + 1;
+    
+    return Math.floor(Math.random()*(4)) + 1
  
  
  }
 
 
- function spin() {
+ function renderMessage() {
 
-    const num1 = getRandomNbr();
-    const num2 = getRandomNbr();
-    const num3 = getRandomNbr();
-    console.log(num1, num2, num3);
+    const num1 = getRandomNbr()
+    const num2 = getRandomNbr()
+    const num3 = getRandomNbr()
+    console.log(num1, num2, num3)
 
-    item1.innerHTML = `${num1}`;
-    item2.innerHTML = `${num2}`;
-    item3.innerHTML = `${num3}`;
+    item1.innerHTML = `${num1}`
+    item2.innerHTML = `${num2}`
+    item3.innerHTML = `${num3}`
      
     if (num1 === num2 && num1 === num3) {
       
-        h2El.innerHTML= "Jackpot!!!";
+        h2El.innerHTML= "Jackpot!!!"
     } else {
-        h2El.innerHTML= "Try again!";
+        h2El.innerHTML= "Try Again!"
     }
     }
- 
+ function hideMessage (){
+ const msg = document.getElementById("hide")
+ msg.style.display = "none"
 
-    
+ }
 
+function reset(){
+    window.location.reload()
 
+}
 function init() {
-
-renderMessage();
-
+reset()
+renderMessage()
+hideMessage()
 }
